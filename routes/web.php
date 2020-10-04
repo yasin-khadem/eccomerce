7faefe35e71c229159b6aee67b443fd2a586f5e0
yasin khadem <yasin.khadem77@gmail.com>
2020-10-04 16:16:27 +0330

    fix spa bugs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/auth/login', function(){
+    return view('home');
+})->name('login');
+
 Route::fallback(function () {
     return view('home');
 });
